b3661706561b58ec87a014f052dce325cf872be4
Sanghamitra Dutta <iradutta02@gmail.com>
2023-11-10 20:41:42 +0530

    Initial commit

---
+++ publisher_list.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"> <!-- Link to your CSS file -->
+    <title>List Of Publishers</title>
+</head>
+<body>
+<?php
+	session_start();
+	require_once "./functions/database_functions.php";
+	$conn = db_connect();
+
+	$query = "SELECT * FROM publisher ORDER BY publisherid";
+	$result = mysqli_query($conn, $query);
+	if(!$result){
+		echo "Can't retrieve data " . mysqli_error($conn);
+		exit;
+	}
+	if(mysqli_num_rows($result) == 0){
+		echo "Empty publisher ! Something wrong! check again";
+		exit;
+	}
+
+	$title = "List Of Publishers";
+	require "./template/header.php";
+?>
+	<p class="lead">List of Publishers</p>
+	<ul>
+	<?php 
+		while($row = mysqli_fetch_assoc($result)){
+			$count = 0; 
+			$query = "SELECT publisherid FROM books";
+			$result2 = mysqli_query($conn, $query);
+			if(!$result2){
+				echo "Can't retrieve data " . mysqli_error($conn);
+				exit;
+			}
+			while ($pubInBook = mysqli_fetch_assoc($result2)){
+				if($pubInBook['publisherid'] == $row['publisherid']){
+					$count++;
+				}
+			}
+	?>
+		<li>
+			<span class="badge"><?php echo $count; ?></span>
+		    <a href="bookPerPub.php?pubid=<?php echo $row['publisherid']; ?>"><?php echo $row['publisher_name']; ?></a>
+		</li>
+	<?php } ?>
+		<li>
+			<a href="books.php">List all the books</a>
+		</li>
+	</ul>
+<?php
+	mysqli_close($conn);
+	require "./template/footer.php";
+?>
+</body>
+</html>
